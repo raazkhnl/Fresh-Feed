83349a2825b8054df9df86df8aa20f889615e3ff
RaaZ Khanal <59169748+raazkhnl@users.noreply.github.com>
2023-05-12 04:28:36 -0700

    Infinite Scroll Completed

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 	static propTypes = {
@@ -10,53 +11,80 @@ export class News extends Component {
 
 	static defaultProps = {
 		category: "general",
+		pageSize: "18"
 	};
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			articles: [],
-			loading: false,
+			loading: true,
 			page: 1,
 		};
-		document.title=`Fresh Feed - ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}`
+		document.title = `Fresh Feed - ${
+			this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)
+		}`;
 	}
-	async updateNews(pageNo){
+	async updateNews(pageNo) {
 		const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=5ed6668292e24f3a8d05038cae087b34&pagesize=${this.props.pageSize}&page=${this.state.page}`;
-		this.setState({ loading: true });
+		// this.setState({ loading: true });
 		let data = fetch(url);
 		let parsedData = await (await data).json();
 		this.setState({
 			articles: parsedData.articles,
 			totalResults: parsedData.totalResults,
-			loading: false
+			loading: false,
 		});
-			
 	}
 
 	async componentDidMount() {
 		this.updateNews();
-
 	}
 
 	prevHandler = async () => {
-		this.setState({page: this.state.page -1});
+		this.setState({ page: this.state.page - 1 });
 		this.updateNews();
 	};
 
 	nextHandler = async () => {
-		this.setState({page: this.state.page + 1});
+		this.setState({ page: this.state.page + 1 });
 		this.updateNews();
 	};
 
+	fetchMoreData = async () => {
+		this.setState({ page: this.state.page + 1 });
+		this.setState({ loading: true });
+		const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=5ed6668292e24f3a8d05038cae087b34&pagesize=${this.props.pageSize}&page=${this.state.page}`;
+		let data = fetch(url);
+		let parsedData = await (await data).json();
+		this.setState({
+			articles: this.state.articles.concat(parsedData.articles),
+			totalResults: parsedData.totalResults,
+			loading: false,
+		});
+	  };
+
 	render() {
 		return (
-			<div className="container my-3">
-				<h2>Fresh Feed - Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h2>
-				{this.state.loading && <Spinner />}
-				<div className="row my-3">
-					{!this.state.loading &&
-						this.state.articles.map((element) => {
+			<>
+				<h2>
+					Fresh Feed - Top{" "}
+					{this.props.category.charAt(0).toUpperCase() +
+						this.props.category.slice(1)}{" "}
+					Headlines
+				</h2>
+				{/* {this.state.loading && <Spinner />} */}
+
+				
+					<InfiniteScroll
+						dataLength={this.state.articles.length}
+						next={this.fetchMoreData}
+						hasMore={this.state.articles.length !== this.state.totalResults}
+						loader={this.state.loading && <Spinner />}
+					>
+						<div className="container my-3">
+						<div className="row my-3">
+						{this.state.articles.map((element) => {
 							return (
 								<div className="col-md-4 my-3" key={element.url}>
 									<NewsItem
@@ -64,15 +92,18 @@ export class News extends Component {
 										description={element.description}
 										imgUrl={element.urlToImage}
 										newsUrl={element.url}
-										  author={element.author}
-										  date={element.publishedAt}
-										  sourceName={element.source.name}
-								/>
+										author={element.author}
+										date={element.publishedAt}
+										sourceName={element.source.name}
+									/>
 								</div>
 							);
 						})}
+						</div>
+						</div>
+					</InfiniteScroll>
 
-					<div className="d-flex justify-content-between">
+					{/* <div className="d-flex justify-content-between">
 						<button
 							disabled={this.state.page <= 1}
 							type="button"
@@ -91,10 +122,12 @@ export class News extends Component {
 						>
 							Next &rarr;
 						</button>
-					</div>
-				</div>
-			</div>
+					</div> */}
+				</>
+				
+			
 		);
+
 	}
 }
 export default News;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ export default class Spinner extends Component {
   render() {
     return (
       <div className="text-center">
-        <img src={loading} alt="loading"></img>
+        <img src={loading} alt="loading" style={{height:"40px"}}></img>
       </div>
     )
   }
